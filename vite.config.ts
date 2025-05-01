424bf455d965e62c7593ca33016590953a7daa4e
andersgMSFT <90765077+andersgMSFT@users.noreply.github.com>
2025-05-01 13:55:08 +0200

    Try new build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+// vite.config.ts
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/summarize-visualizer/' : '/',
   plugins: [react()],
-})
+}))
