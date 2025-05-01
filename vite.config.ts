083e274a891689d564349c169c8949bb833bca0b
Anders Nørskov <andersg@microsoft.com>
2025-05-01 14:59:04 +0200

    fix vite config and deploy.yaml

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   plugins: [react(), viteSingleFile()],
   build: {
     assetsInlineLimit: Infinity, // this is enough
