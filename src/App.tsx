a60228bb31ab9be1b5e8b92177200101dac67a3c
andersgMSFT <90765077+andersgMSFT@users.noreply.github.com>
2025-05-12 09:18:44 +0200

    update version

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import {
 import { InputPageDataCard } from "./Components/InputDataCard/InputPageDataCard";
 import TestSummaryCard from "./Components/TestSummary/TestSummaryCard";
 
-const version = "1.0.1";
+const version = "0.0.2";
 
 enum AppState {
   WaitingForUpload = "waitingforupload",
